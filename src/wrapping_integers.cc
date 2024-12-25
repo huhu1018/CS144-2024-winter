a3638c1cc0d6049e550f7bcd1caf4496c7332b38
huhu <huhu1018@mail.ustc.edu.cn>
2024-12-25 17:35:16 +0800

    [lab2] 1.finish wrapping_integers

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,19 +1,21 @@
 #include "wrapping_integers.hh"
-
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  // zero_point + absolute_seq mod 2^32 == seqno
+  return Wrap32 { zero_point + n };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint64_t upper = static_cast<uint64_t>( UINT32_MAX ) + 1;
+  uint32_t ckpt_mod = Wrap32::wrap( checkpoint, zero_point ).raw_value_;
+  uint32_t distance = raw_value_ - ckpt_mod;
+  if ( distance <= ( upper >> 1 ) || checkpoint + distance < upper ) {
+    return checkpoint + distance;
+  }
+  return checkpoint + distance - upper;
 }
